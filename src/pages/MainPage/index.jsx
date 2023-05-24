a8e8ab2e36533db59536b6994442e4fe012a39c3
JussVi <osadze_denis_007@mail.ru>
2023-05-24 17:31:59 +0600

    First commit

---
+++ src/pages/MainPage/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import './style.css';
+
+function MainPage() {
+  return (
+    <div className='main-page-wrap'>
+        <div className="container">        
+            <div className="main-block">
+                <div className="main-block-text-info">
+                    <h1 className='main-block-header'>
+                        Lorem ipsum dolor sit amet.
+                    </h1>
+                    <p className="main-block-subheader">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, unde cum nemo sit quaerat vel!
+                    </p>
+                </div>
+                <span className='main-block-img'></span>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default MainPage
