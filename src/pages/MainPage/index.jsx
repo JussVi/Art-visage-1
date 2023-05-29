05a35feaa18bc8c26c230eac688937fe00f04747
Oscarquell <oscarquell96@gmail.com>
2023-05-29 14:13:14 +0600

    Add strapi

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -1,29 +1,62 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import FirstSection from "../../components/sections-of-main-page/first-section";
 import SecondSection from "../../components/sections-of-main-page/second-section";
 import ThirdSections from "../../components/sections-of-main-page/third-sections";
+import FourthSection from "../../components/sections-of-main-page/fourth-section";
+import axios from "axios";
 import './style.css';
+import CircularIndeterminate from "../../components/loader";
 
 
 function MainPage() {
 
+    const [data, setData] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    const fetchData = async () => {
+        setIsLoading(true)
+        try {
+            const response = await axios.get('http://localhost:1337/api/masters')
+            setData(response.data.data)
+        } catch (error) {
+            alert('error')
+        } finally {
+            setIsLoading(false)
+        }
+    }
 
   return (
-    <div className='main-page-wrap'>
-      <div className='scroll-wrap'>
-        <FirstSection />
-      </div>
-      <div className='scroll-wrap'>
-        <SecondSection />
-      </div>
-      <div className='scroll-wrap'>
-        <ThirdSections />
-      </div>
-      <div className='scroll-wrap'>
-        <div className="footer"></div>
-      </div>
+        <>
+            { isLoading ?
+                <CircularIndeterminate />
+                :
+                <div className='main-page-wrap'>
+                    <div className='scroll-wrap'>
+                        <FirstSection />
+                    </div>
+                    <div className='scroll-wrap'>
+                        <SecondSection />
+                    </div>
+                    <div className='scroll-wrap'>
+                        <ThirdSections />
+                    </div>
+                    <div className='scroll-wrap'>
+                        <FourthSection
+                            data={data}
+                        />
+                    </div>
+                    <div className='scroll-wrap'>
 
-    </div>
+                        <div className="footer"></div>
+                    </div>
+
+                </div>
+            }
+        </>
   )
 }
 
