39e31332c23f2d5c44621cda17e0435eaac65664
Oscarquell <oscarquell96@gmail.com>
2023-06-30 00:27:42 +0600

    Added modal window

--- src/components/sections-of-main-page/forth-section.jsx
+++ src/components/sections-of-main-page/forth-section.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Anton from '../../visual/img/Anton.png'
 import Zlata from '../../visual/img/Zlata.png'
 import Zalina from '../../visual/img/Zalina.png'
 import Olga from '../../visual/img/Olga.png'
 import Aisuluu from '../../visual/img/Aisuluu.png'
 import Damira from '../../visual/img/Damira.png'
+import ModalWindow from "../modalWindow";
 import '../../pages/MainPage/style.css'
 
 const FourthSection = () => {
+
+    const [isModalActive, setIsModalActive] = useState(false)
+
     return (
         <div className="info-block">
             <div className="container">
@@ -15,43 +19,48 @@ const FourthSection = () => {
                    Мастеры
                 </h2>
                 <div className="main-block-masters-cards">
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Anton} alt=""/>
-                        <span className='card-price'>
+                        <span className='name-of-master'>
                             Антон Шугальский
                         </span>
                     </div>
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Zlata} alt=""/>
-                        <div className='card-price'>
+                        <div className='name-of-master'>
                             Злата Зорова
                         </div>
                     </div>
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Zalina} alt=""/>
-                        <span className='card-price'>
+                        <span className='name-of-master'>
                             Залина Хасанова
                         </span>
                     </div>
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Olga} alt=""/>
-                        <span className='card-price'>
+                        <span className='name-of-master'>
                            Ольга Дороганова
                         </span>
                     </div>
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Aisuluu} alt=""/>
-                        <span className='card-price'>
+                        <span className='name-of-master'>
                             Айсулуу Султаналиева
                         </span>
                     </div>
-                    <div className="photo-of-master">
+                    <div onClick={() => setIsModalActive(true)} className="photo-of-master">
                         <img className='photo-of-master-img' src={Damira} alt=""/>
-                        <span className='card-price'>
+                        <span className='name-of-master'>
                             Дамира Шакирова
                         </span>
                     </div>
                 </div>
+                <ModalWindow
+                    isModalActive={isModalActive}
+                    setIsModalActive={setIsModalActive}>
+
+                </ModalWindow>
             </div>
         </div>
     );
