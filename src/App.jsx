b33cc2794138d31e4dd4f68b3d5adb50d5432f7c
JussVi <osadze_denis_007@mail.ru>
2023-07-03 14:42:19 +0600

    Add CoursesPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import MainPage from "./pages/MainPage";
+import CoursesPage from './pages/CoursesPage';
 import {ParallaxProvider} from "react-scroll-parallax";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <ParallaxProvider>
       <div className="App">
-        <MainPage />
+        <Switch>
+          <Route exact path="/" component={MainPage} />
+          <Route exact path="/courses" component={CoursesPage} />
+        </Switch>
       </div>
     </ParallaxProvider>
   );
